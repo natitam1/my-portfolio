a879e8310d26bad7f4becf8f9d7b196b9fed6066
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-05-07 20:22:42 +0300

    Initial commit of my portfolio

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -5,7 +5,7 @@ const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
       <img
-        src="../../public/images/email-image.jpg"
+        src="/images/email-image.jpg"
         alt="email image"
         className="max-w-[300px]"
       />
